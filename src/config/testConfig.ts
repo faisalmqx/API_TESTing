db23e47dae806fd16095bbbe503bf04414954b42
sunny prajapati <sunny@marqetrix.com>
2023-11-14 11:20:24 +0530

    added new reporter

--- src/config/testConfig.ts
+++ src/config/testConfig.ts
@@ -1,9 +1,11 @@
 import * as newman from 'newman';
 import { ENVIRONMENT } from '../data/globalData';
 import logger from '../support/logger';
-
+import path from 'path';
 // collection_file path imported from postman
+// const collection_Path = 'src/data/collections/newman-test-sample.postman_collection.json';
 const collection_Path = 'src/data/collections/PFW_V2_Collection_GET_CALL.postman_collection.json';
+// const collection_Path = 'src/data/collections/PFW_V2 Tests.postman_collection.json';
 let environment_Path: string;
 
 // environment_Path is path for environemnt.json file imported from postman
@@ -28,13 +30,23 @@ switch(ENVIRONMENT){
         environment_Path = 'src/data/environments/Local.postman_environment.json'
     }
 }
+
+const reportsDirectory = path.join(__dirname, '../reports');
 // IIFE function will get executed automatically no need to call it explicitly
 (
      () => {
         newman.run({
             collection: collection_Path,
             environment: environment_Path,
-            reporters: 'cli',
+            reporters: ['cli','json','htmlextra'],
+            reporter: {
+                htmlextra: {
+                     export: 'src/reports/report.html',
+                },
+                json: {
+                    export: 'src/reports/newman-report.json'// Specify the file to which the JSON report will be written
+                }
+            },
             insecure: true,
         }).on('start',  (err, args) =>{
             if(err || args.error){
@@ -47,8 +59,14 @@ switch(ENVIRONMENT){
             if(err){
                 throw new Error(err);
             }
-            else{
-                logger.info('collection ran successfully!');
+            else {
+                if (summary.run.failures.length > 0) {
+                    logger.error('There were test failures.');
+                    // Here you can handle the failures as needed, for example:
+                    // Write a summary of failures to a file, send a notification, etc.
+                } else {
+                    logger.info('All tests passed!');
+                }
             }
         })
     }
